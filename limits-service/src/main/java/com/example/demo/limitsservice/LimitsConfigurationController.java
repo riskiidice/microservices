c04d75b862edea988cbe0b192a3abcfd0b474b69
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2018-04-18 13:45:16 +0700

    First Commit

---
+++ limits-service/src/main/java/com/example/demo/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package com.example.demo.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsConfiguration() {
+		return new LimitConfiguration(configuration.getMaximum(), 
+				configuration.getMinimum());
+	}
+}
